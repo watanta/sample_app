b8691b8dbe86ba299df0e91599587de009da103f
watanta <ec2-user@ip-172-31-31-51.us-west-2.compute.internal>
2018-06-05 13:56:41 +0000

    implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -9,6 +11,11 @@ Rails.application.routes.draw do
   
   get "/signup", to:"users#new"
   post "signup", to: "users#create"
+  
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
